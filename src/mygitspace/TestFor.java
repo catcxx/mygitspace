d971aff07fd04c709733d82a4fa0ab40700ab7d5
catcxx <frankchen443@gmail.com>
2016-04-10 23:50:15 +0800

    fisrt commit on eclipse

---
+++ src/mygitspace/TestFor.java
@@ -0,0 +1,22 @@
+package mygitspace;
+
+public class TestFor {
+public static void main(String[] args){
+	for(int i=1;i<=1000;i++){
+		if(i%5==0){
+		System.out.print(i+"  ");
+		}
+		if(i%15==0){
+			System.out.println();//Ã¿ÐÐ3¸ö
+		}
+	}
+	
+	/*System.out.println();
+	int a=1;
+	while(a<=10){
+		System.out.print(a+" ");
+		a++;
+		
+	}*/
+}
+}
